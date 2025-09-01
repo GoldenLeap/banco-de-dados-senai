cd98a01486f382da1ce986814f0ce5a227a03ecf
Gabriel Bernardi <115042820+GoldenLeap@users.noreply.github.com>
2025-09-01 16:46:39 -0300

    correção

--- Aula 04/Atividade avaliativa.sql
+++ Aula 04/Atividade avaliativa.sql
@@ -3,47 +3,70 @@ CREATE DATABASE atividadeavaliativa1;
 USE atividadeavaliativa1;
 
 CREATE TABLE fornecedor(
-	f_codigo INT AUTO_INCREMENT PRIMARY KEY,
-    f_nome VARCHAR(100),
-	f_status VARCHAR(11),
+	fcodigo INT AUTO_INCREMENT PRIMARY KEY,
+    fnome VARCHAR(100),
+	sstatus VARCHAR(11),
     cidade VARCHAR(100)
 );
 
 CREATE TABLE peca(
-	p_codigo INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+	pcodigo INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
 	cor VARCHAR(20) NOT NULL,
     peso DECIMAL(2,2) NOT NULL,
     cidade VARCHAR(100) NOT NULL,
-    p_nome VARCHAR(100) NOT NULL
+    pnome VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE instituicao(
-	i_codigo INT PRIMARY KEY AUTO_INCREMENT,
+	icodigo INT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(100)
 );
 
 CREATE TABLE projeto(
-	pr_codigo INT PRIMARY KEY AUTO_INCREMENT,
+	prcod INT PRIMARY KEY AUTO_INCREMENT,
     cidade VARCHAR(100),
-    i_codigo INT,
-    FOREIGN KEY (i_codigo) REFERENCES instituicao(i_codigo)
+    prnome VARCHAR(100),
+    icodigo INT,
+    FOREIGN KEY (icodigo) REFERENCES instituicao(icodigo)
 );
 
 CREATE TABLE Fornecimento(
-	id_fornecimento INT PRIMARY KEY AUTO_INCREMENT,
-    pr_codigo INT,
-    f_codigo INT,
-    p_codigo INT,
-    FOREIGN KEY (pr_codigo) REFERENCES projeto(pr_codigo),
-    FOREIGN KEY (f_codigo) REFERENCES fornecedor(f_codigo),
-    FOREIGN KEY (p_codigo) REFERENCES peca(p_codigo)
+	idfornecimento INT PRIMARY KEY AUTO_INCREMENT,
+    prcod INT,
+    fcodigo INT,
+    pcodigo INT,
+    FOREIGN KEY (prcod) REFERENCES projeto(prcod),
+    FOREIGN KEY (fcodigo) REFERENCES fornecedor(fcodigo),
+    FOREIGN KEY (pcodigo) REFERENCES peca(pcodigo)
 );
 -- EX 2
-ALTER TABLE fornecedor
-CHANGE f_codigo f_cod INT;
+ALTER TABLE `atividadeavaliativa1`.`fornecedor` 
+CHANGE COLUMN `fcodigo` `fcod` INT NOT NULL AUTO_INCREMENT ;
 
 ALTER TABLE Instituicao RENAME cidade;
-ALTER TABLE cidade CHANGE
-i_codigo ccod INT,
+
+ALTER TABLE cidade CHANGE COLUMN
+`icodigo` `ccod` INT NOT NULL AUTO_INCREMENT,
 CHANGE nome cnome VARCHAR(100),
 ADD uf VARCHAR(2);
+
+ALTER TABLE `atividadeavaliativa1`.`peca` 
+CHANGE COLUMN `pcodigo` `pcod` INT NOT NULL AUTO_INCREMENT,
+CHANGE pnome pnome VARCHAR(100), 
+ADD ccod INT, 
+ADD FOREIGN KEY (ccod) REFERENCES cidade(ccod);
+
+ALTER TABLE Projeto
+drop cidade,
+RENAME COLUMN `icodigo` TO `ccod`;
+
+ALTER TABLE fornecimento
+RENAME COLUMN `fcodigo` TO `fcod`,
+RENAME COLUMN `pcodigo`TO `pcod`,
+ADD COLUMN `Quantidade` int;
+
+CREATE UNIQUE INDEX idx_pcod ON peca(pcod);
+CREATE UNIQUE INDEX idx_ccod ON cidade(ccod);
+CREATE UNIQUE INDEX idx_fornecimento_idforn ON fornecimento(idfornecimento);
+
+
