cc0467be4cc2b11fad23b29c60f03412b3e4adad
Gabriel Bernardi <115042820+GoldenLeap@users.noreply.github.com>
2025-09-29 16:43:45 -0300

    Avaliação Somativa - Modelagem no brmodelo

---
+++ Somativa/Modelo Fisico Somativa.sql
@@ -0,0 +1,37 @@
+-- Geração de Modelo físico
+-- Sql ANSI 2003 - brModelo.
+
+
+
+CREATE TABLE Avaliacoes (
+nota DECIMAL(4,2) NOT NULL ,
+comentario VARCHAR(255) NOT NULL,
+id_avaliacao INT NOT NULL PRIMARY KEY PRIMARY KEY,
+id_inscricao INT PRIMARY KEY NOT NULL AUTO_INCREMENT
+)
+
+CREATE TABLE Cursos (
+id_curso INT PRIMARY KEY NOT NULL AUTO_INCREMENT PRIMARY KEY,
+descricao VARCHAR(255) NOT NULL,
+carga_horaria INT NOT NULL,
+titulo VARCHAR(100) NOT NULL,
+status BOOL NOT NULL
+)
+
+CREATE TABLE Alunos (
+nome VARCHAR(200) NOT NULL,
+email VARCHAR(50) NOT NULL,
+data_nascimento DATE NOT NULL,
+id_aluno INT PRIMARY KEY NOT NULL AUTO_INCREMENT PRIMARY KEY
+)
+
+CREATE TABLE Inscricoes (
+id_inscricao INT PRIMARY KEY NOT NULL AUTO_INCREMENT PRIMARY KEY,
+data_inscricao DATE NOT NULL,
+id_curso INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+id_aluno INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+FOREIGN KEY(id_curso) REFERENCES Cursos (id_curso),
+FOREIGN KEY(id_aluno) REFERENCES Alunos (id_aluno)
+)
+
+ALTER TABLE Avaliacoes ADD FOREIGN KEY(id_inscricao) REFERENCES Inscricoes (id_inscricao)
