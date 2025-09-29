f51306b955dcc20bcbbc07a75c2f3b80bfd99bb6
Gabriel Bernardi <115042820+GoldenLeap@users.noreply.github.com>
2025-09-29 16:55:51 -0300

    inserção de dados em progresso

---
+++ Somativa/Somativa inserção de dados.sql
@@ -0,0 +1,7 @@
+USE somativa;
+INSERT INTO Alunos(nome, email, data_nascimento) VALUES
+	("Cleber da Silva", "clebinsilvana@teste.com", "2005-02-12"),
+    ("Ana Clara de Souza", "anaclara@teste.com", "2004-05-31"),
+    ("Vinicius da Silva", "vini.silva@teste.com", "1990-06-24"),
+    ("Carla Soares", "carla.soares@teste.com", "1995-05-10"),
+    ("Wanderley da Silva", "wandeba.silva@teste.com", "2000-05-10");
